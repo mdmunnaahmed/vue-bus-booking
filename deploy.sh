45a239c21b0479921f6e0e54b0ed941305cd8076
mdmunnaahmed <msdmunna77@gmail.com>
2023-08-16 10:36:11 +0600

    new deployment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#1/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+git add -A
+git commit -m "new deployment"
+git push -f https://github.com/mdmunnaahmed/vue-bus-booking.git main:gh-pages
+
+cd -
